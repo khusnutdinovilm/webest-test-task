f15037730edf02858ba27c2e91aa0734b765ecba
ilmir khusnutdinov <ilmir.khusnutdinov.kh@gmail.com>
2025-05-27 13:58:11 +0500

    CONFIG: настройка линтинга, форматирования и алиас-путей
    - добавил правила форматирования;
    - настроил алиас-пути, которые могут пригодится;
    - добавила правила линтинга, включая порядок импортов.

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,7 +1,11 @@
-import { globalIgnores } from 'eslint/config'
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
-import pluginVue from 'eslint-plugin-vue'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import eslintPluginImport from "eslint-plugin-import";
+import pluginVue from "eslint-plugin-vue";
+
+import skipFormatting from "@vue/eslint-config-prettier/skip-formatting";
+import { defineConfigWithVueTs, vueTsConfigs } from "@vue/eslint-config-typescript";
+import { globalIgnores } from "eslint/config";
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
 // import { configureVueProject } from '@vue/eslint-config-typescript'
@@ -10,13 +14,155 @@ import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
 export default defineConfigWithVueTs(
   {
-    name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}'],
+    name: "app/files-to-lint",
+    files: ["**/*.{ts,mts,tsx,vue}"],
   },
 
-  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+  globalIgnores(["**/dist/**", "**/dist-ssr/**", "**/coverage/**"]),
 
-  pluginVue.configs['flat/essential'],
+  pluginVue.configs["flat/essential"],
   vueTsConfigs.recommended,
   skipFormatting,
-)
+
+  {
+    plugins: {
+      eslintPluginImport,
+    },
+  },
+
+  {
+    rules: {
+      "vue/block-order": [
+        "error",
+        {
+          order: ["template", "script", "script[setup]", ["style[scoped]", "style"]],
+        },
+      ],
+      "vue/component-definition-name-casing": ["error", "kebab-case"],
+      "vue/attributes-order": [
+        "error",
+        {
+          order: [
+            "DEFINITION",
+            "LIST_RENDERING",
+            "CONDITIONALS",
+            "RENDER_MODIFIERS",
+            "GLOBAL",
+            "UNIQUE",
+            "SLOT",
+            "TWO_WAY_BINDING",
+            "OTHER_DIRECTIVES",
+            ["ATTR_DYNAMIC", "ATTR_STATIC", "ATTR_SHORTHAND_BOOL"],
+            "EVENTS",
+            "CONTENT",
+          ],
+        },
+      ],
+      "vue/multi-word-component-names": "error",
+      "vue/no-v-html": "warn",
+      "vue/v-slot-style": ["error", "shorthand"],
+      "vue/component-name-in-template-casing": ["error", "kebab-case"],
+      "vue/custom-event-name-casing": ["error", "kebab-case"],
+      "eslintPluginImport/no-relative-parent-imports": "error",
+      "eslintPluginImport/order": [
+        "error",
+        {
+          "groups": [
+            "builtin",
+            "external",
+            "internal",
+            ["parent", "sibling", "index"],
+            "object",
+            "type",
+          ],
+          "pathGroups": [
+            {
+              pattern: "*",
+              group: "external",
+              position: "before",
+            },
+            {
+              pattern: "*",
+              group: "builtin",
+              position: "before",
+            },
+            {
+              pattern: "api/**",
+              group: "internal",
+              position: "after",
+            },
+            {
+              pattern: "store/**",
+              group: "internal",
+              position: "after",
+            },
+            {
+              pattern: "assets/**",
+              group: "internal",
+              position: "after",
+            },
+            {
+              pattern: "styles/**",
+              group: "internal",
+              position: "after",
+            },
+            {
+              pattern: "icons/**",
+              group: "internal",
+              position: "after",
+            },
+            {
+              pattern: "common/**",
+              group: "internal",
+              position: "after",
+            },
+            {
+              pattern: "shared/**",
+              group: "internal",
+              position: "after",
+            },
+            {
+              pattern: "ui/**",
+              group: "internal",
+              position: "after",
+            },
+            {
+              pattern: "layouts/**",
+              group: "internal",
+              position: "after",
+            },
+            {
+              pattern: "pages/**",
+              group: "internal",
+              position: "after",
+            },
+            {
+              pattern: "modules/**",
+              group: "internal",
+              position: "after",
+            },
+            {
+              pattern: "composable/**",
+              group: "internal",
+              position: "after",
+            },
+            {
+              pattern: "utils/**",
+              group: "internal",
+              position: "after",
+            },
+          ],
+          "pathGroupsExcludedImportTypes": ["builtin"],
+          "newlines-between": "always",
+          "alphabetize": {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+
+      "@typescript-eslint/no-wrapper-object-types": "off",
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,31 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vueDevTools from "vite-plugin-vue-devtools";
+
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/webest-test-task/' : '/',
+  base: process.env.NODE_ENV === "production" ? "/webest-test-task/" : "/",
+
   plugins: [vue(), vueDevTools()],
+
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      api: fileURLToPath(new URL("./src/api/", import.meta.url)),
+      layouts: fileURLToPath(new URL("./src/layouts/", import.meta.url)),
+      pages: fileURLToPath(new URL("./src/pages/", import.meta.url)),
+      assets: fileURLToPath(new URL("./src/assets/", import.meta.url)),
+      styles: fileURLToPath(new URL("./src/styles/", import.meta.url)),
+      icons: fileURLToPath(new URL("./src/components/icons/", import.meta.url)),
+      common: fileURLToPath(new URL("./src/components/common/", import.meta.url)),
+      shared: fileURLToPath(new URL("./src/components/shared/", import.meta.url)),
+      ui: fileURLToPath(new URL("./src/components/ui/", import.meta.url)),
+      modules: fileURLToPath(new URL("./src/modules/", import.meta.url)),
+      composable: fileURLToPath(new URL("./src/composable/", import.meta.url)),
+      utils: fileURLToPath(new URL("./src/utils/", import.meta.url)),
+      store: fileURLToPath(new URL("./src/store/", import.meta.url)),
     },
   },
-})
+});
