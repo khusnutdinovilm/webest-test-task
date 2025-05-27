4bcff94d04ac057f1301149c17b9fff89c137522
ilmir khusnutdinov <ilmir.khusnutdinov.kh@gmail.com>
2025-05-27 14:56:37 +0500

    ADD: разработал шапку сайта

---
+++ src/components/common/app-header/app-header.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class="app-header">
+    <div class="container">
+      <div class="app-header__wrapper">
+        <app-brand class="app-header__brand" />
+      </div>
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts">
+import AppBrand from "common/app-brand";
+
+defineOptions({
+  name: "app-header",
+});
+</script>
---
+++ src/components/common/app-header/index.ts
@@ -0,0 +1,4 @@
+import "./app-header.scss";
+import AppHeader from "./app-header.vue";
+
+export default AppHeader;
