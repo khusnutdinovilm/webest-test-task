d169c0604c5204858dedea32b946b2729c7cbf90
ilmir khusnutdinov <ilmir.khusnutdinov.kh@gmail.com>
2025-05-27 21:37:11 +0500

    UPD: добавил компонент поиска в шапку сайта

--- src/components/common/app-header/app-header.vue
+++ src/components/common/app-header/app-header.vue
@@ -3,6 +3,8 @@
     <div class="container">
       <div class="app-header__wrapper">
         <app-brand class="app-header__brand" />
+
+        <search-bar class="app-header__search-bar" />
       </div>
     </div>
   </header>
@@ -10,6 +12,7 @@
 
 <script setup lang="ts">
 import AppBrand from "common/app-brand";
+import SearchBar from "common/search-bar";
 
 defineOptions({
   name: "app-header",
