60e586bc7ae707b6fc18015fd330d9a13848513e
ilmir khusnutdinov <ilmir.khusnutdinov.kh@gmail.com>
2025-05-27 14:56:52 +0500

    UPD: вывел шапку сайта на страницу

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,24 @@
 <template>
-  <router-view />
+  <app-header />
+
+  <main>
+    <router-view />
+  </main>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppHeader from "common/app-header";
+</script>
 
-<style></style>
+<style lang="scss">
+#app {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  min-height: 100dvh;
+
+  & main {
+    flex: 1;
+  }
+}
+</style>
